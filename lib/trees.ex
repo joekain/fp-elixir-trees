71102e5c75c5f046bc1181f8e0e58cddeb3f26fd
Joseph Kain <joekain@gmail.com>
2014-06-29 16:13:15 -0700

    Size test and constructors

--- lib/trees.ex
+++ lib/trees.ex
@@ -1,3 +1,8 @@
-defmodule Trees do
+defmodule Tree do
+  def leaf(v), do: { :leaf, v }
+  def branch(left, right), do: { :branch, left, right }
+
+  def tree_size({:leaf, _}), do: 1
+  def tree_size({:branch, left, right}), do: 1 + tree_size(left) + tree_size(right)
 
 end
--- test/trees_test.exs
+++ test/trees_test.exs
@@ -1,7 +1,33 @@
 defmodule TreesTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  def build_tree do
+    Tree.branch(
+      Tree.branch(
+        Tree.branch(
+          Tree.leaf(6),
+          Tree.branch(
+            Tree.leaf(5),
+            Tree.leaf(4)
+          )
+        ),
+        Tree.leaf(3)
+      ),
+      Tree.branch(
+        Tree.branch(
+          Tree.branch(
+            Tree.leaf(2),
+            Tree.leaf(1)
+          ),
+          Tree.leaf(0)
+        ),
+        Tree.leaf(0)
+      )
+    )
   end
+
+  test "tree_size should count all nodes" do
+    assert Tree.tree_size(build_tree) == 15
+  end
+
 end
